b3ca4dbbd0f4810be5d440db0df4780a5a7dfa02
AcoderRose <addyro96@att.net>
2024-05-13 16:14:43 -0700

    Made changes to index.html and script.js mainly, site was getting weather data just fine but now running into some errors retrieving weather data, I was able to get my local storage to work correctly however will troubleshoot with my new api key and check connection to open weather api site.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,90 @@
-const apiKey = "37506fc774616b79f481bc290df83ee5";
-const searchEl = document.getElementById("form");
-// Make a function to get the current weather.
-function getCurrentWeather(cityName) {
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
-  //   make a fetch request to get the weather data.
-  // put the weather data on the page
-}
-// Make a function to get the five day forecast.
-function getForecast() {
-//   https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+// API key and base URL inspired variables
+const apiKey = `0a0b2d9a5420001c36f0b50bb2752ee3`;
+let city = "";
+// Retrieval of search history from local storage inspired variables
+let searchedHistory =
+  JSON.parse(localStorage.getItem("searched-history")) || [];
+// Reference of html elements inspired variables
+const formEl = document.getElementById("form");
+const searchBar = document.getElementById("searchsection");
+const searchHistoryEl = document.getElementById("searched-history");
+const dashboardEl = document.getElementById("dashboard");
+const cardEl = document.getElementsByClassName("forecast-card");
+const submitBtn = document.querySelector("#submitted");
+
+// function to get cities' latitude and longitude data
+function getCityPin(city, apiKey) {
+  fetch(
+    `https://api.openweathermap.org/geo/1.0/direct?q=${city},&limit=1&appid=${apiKey}`
+  )
+    // JSON conversion
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      // Was data returned?
+      if (data.length === 0) {
+        alert(
+          "Oops no city found, Please check spelling or try searching for a different city please."
+        );
+      } else {
+        // latitude and longitude data retrieved
+        let lat = data[0].lat;
+        let lon = data[0].lon;
+        // Calling to open weather API function to forecast
+        getForecast(lat, lon, apiKey, city);
+      }
+    });
 }
 
-searchEl.addEventListener("submit", function (event) {
+// function to get forecast data
+function getForecast(lat, lon, apiKey, city) {
+  let currentDate = new Date().toLocaleDateString();
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
+  )
+    // JSON conversion
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      // Any weather data available?
+      if (data.length === 0) {
+        alert("Oops, no weather information can be found at this time.");
+      } else {
+        // calling of the updateForecast function
+        updateForecast(data, city, currentDate);
+        // calling of the updatedDashboard function
+        updateDashboard(data);
+      }
+    })
+    .catch(function (error) {
+      console.error(
+        "Error occurred while attempting to fetch weather data:",
+        error
+      );
+      alert("Error occurred while attempting to fetch weather data.");
+    });
+}
+
+// Function to update form entry
+function updateCitySearch(event) {
   event.preventDefault();
-  const cityName = document.getElementById("search-bar").value;
-  getCurrentWeather(cityName);
-  // get weather information by city
-});
+  // Retrieves city name from search entry and removes any mistakenly entered by user extra spaces.
+  city = searchBar.value.trim();
+  if (city) {
+    // Is search bar empty?
+    getCityPin(city, apiKey);
+    searchBar.value = "";
+    searchedHistory.push(city);
+    // Storing of updated searchedHistory in localStorage
+    localStorage.setItem("searched-history", JSON.stringify(searchedHistory));
+    // Last searched city stored
+    localStorage.setItem("last-searched", city);
+    // Searched history display is updated
+    displaySearch();
+  }
+}
+
+// event listeners
+formEl.addEventListener("submit", updateCitySearch);
